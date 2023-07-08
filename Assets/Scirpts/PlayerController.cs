2bf4b4ba0d6362a372aedc4125184352c48b72cb
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-08 22:49:57 +0500

    Add ability to move our ball to left right back and forward If the ball falls down we load the Game scene

--- Assets/Scirpts/PlayerController.cs
+++ Assets/Scirpts/PlayerController.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // To Manage Scenes
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed; // speed for moving the ball - in Inspector it's 15
+
+    Rigidbody rb;
+
+    float xInput;
+    float yInput;
+    
+
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +29,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if ball fals down reload Game scene
+        if (transform.position.y < -5f) 
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        // Assign Direction movement to left/right and back/forward
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        // Apply a force to this Rigidbody in direction of this GameObject to left/right and back/forward
+        rb.AddForce(xInput * speed, 0, yInput * speed); //
     }
 }
